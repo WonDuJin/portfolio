125ad5755ddc1e4d5a5989fa2bec81c4f5d01eaa
WonDuJin <windyjan1@naver.com>
2022-11-18 00:46:09 +0900

    Who Am I?

---
+++ backend/backend.py
@@ -0,0 +1,1 @@
+print('Who Am i?')
---
+++ src/App/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>        
+        <h1>
+          Who Am I ?
+        </h1>        
+      </header>
+    </div>
+  );
+}
+
+export default App;
