88a56cb3693e5ba09a24c2713dbb56de79737b44
WonDuJin <windyjan1@naver.com>
2022-12-30 11:01:16 +0900

    웹페이지에 간단한 텍스트 출력

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const check = ()=>{
+  console.log("나는 뉴구?")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>나는 뉴규?</div>
+    </>
+  )
 }
 
-export default App;
+check()
+
+export default check;
